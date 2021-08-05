f17c4492896875a08ac6e07d8584a7b9a7a5e4cc
Tyler Seamans <55858751+tseamans21@users.noreply.github.com>
2021-08-05 14:15:36 -0600

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -36,7 +36,67 @@ router.post('/save', (req, res) => {
         })
     });
 
-
+    router.put('/update', (req, res) => { 
+        const Business = req.body.smallBusiness
+        const newAddress = req.body.address 
+        const newWebsite = req.body.website 
+        const newCompanyTech = req.body.companyTech 
+        const newPoc = req.body.poc
+        const newEmailInfo = req.body.emailinfo
+        const newPhoneInfo = req.body.phoneinfo
+        const newTitleSttr = req.body.titleSTTR
+        const newDescripSttr = req.body.descripSTTR
+        const newPrincInv = req.body.princInv
+        const newBusinessSplit = req.body.businessSplit
+        const newCctiSplit = req.body.cctiSplit
+        const newCctiProvide = req.body.cctiprovide
+        const newMou = req.body.mou
+        const newNda = req.body.nda
+        const newIpAdd = req.body.ipADD
+        const newCycleSubmit = req.body.cycleSubmit
+        const newTopicID = req.body.topicID
+        const newSttrId = req.body.sttrID
+        const newPhaseType = req.body.phaseType
+        const newStateOfProject = req.body.stateOfProject
+        const filter = {smallBusiness: Business}
+        const update = {
+            address: newAddress,
+            website: newWebsite, 
+            companyTech: newCompanyTech,
+            poc: newPoc,
+            emailinfo: newEmailInfo,
+            phoneinfo: newPhoneInfo, 
+            titleSTTR: newTitleSttr,
+            descripSTTR: newDescripSttr,
+            princInv: newPrincInv,
+            businessSplit: newBusinessSplit,
+            cctiSplit: newCctiSplit,
+            cctiprovide: newCctiProvide,
+            mou: newMou,
+            nda: newNda,
+            ipADD: newIpAdd,
+            cycleSubmit: newCycleSubmit,
+            topicID: newTopicID,
+            sttrID: newSttrId,
+            phaseType: newPhaseType,
+            stateOfProject: newStateOfProject
+        }
+        try { 
+            // This is our second attempt using the findOneAndUpdate function. This make it where we do
+            // not need to find the ID field. 
+            blogpost.findOneAndUpdate(filter, update)
+            .then((data) => {
+                console.log('Debug statement => Data: ', data);
+                res.json(data);
+            })
+            .catch((error) => {
+                console.log('error: ', error);
+            });
+        } catch (err) { 
+            console.log(err)
+        }
+        res.send('Updated!');
+    });
     
 
 //blank route for testing
